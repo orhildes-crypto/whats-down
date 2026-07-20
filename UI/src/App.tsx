322c333959632a3f3955e0aa99f6a0d1cc5e3227
or hildesheim <orhildes@email.com>
2026-07-20 17:28:18 +0300

    (feat): Adding vite proxy to be able to send requests to backend (because sameSite = lax)

---
+++ UI/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      What's Down
+    </div>
+  )
+}
+
+export default App;
--- UI/src/main.tsx
+++ UI/src/main.tsx
@@ -1,1 +1,9 @@
-trash:///App.tsx
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
